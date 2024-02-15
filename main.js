219ec3c045fce0572430036f23122d0382a29b1f
Max <139768623+zerotul777@users.noreply.github.com>
2024-02-15 13:00:05 +0300

    +hidden-password

--- main.js
+++ main.js
@@ -29,3 +29,16 @@ function openPopup() {
 function closePopup() {
     popup.classList.remove('open-popup')
 }
+
+let eyeicon = document.getElementById('eyeicon')
+let password = document.getElementById('password')
+
+eyeicon.onclick = function() {
+    if (password.type == 'password') {
+        password.type = 'text';
+        eyeicon.src = './images/eye-open.png'
+    } else {
+        password.type = 'password';
+        eyeicon.src = './images/eye-close.png'
+    }
+}
